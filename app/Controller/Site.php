ee1242b817b803475603beaabcaa96c524d4d761
DESKTOP-QL9B2DK\DimASSik <goose9098@gmail.com>
2025-03-26 14:11:58 +0300

    Изменил путь в path.php , переписал код функции хеллоу,теперь она тоже рендерится

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -7,11 +7,12 @@ class Site
     public function index()
     {
         $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
+        return $view->render('site/hello', ['message' => 'index working']);
 
     }
     public function hello()
     {
-        return new View('site.hello', ['message' => 'hello working']);
+        $view = new View();
+        return $view->render('site/hello', ['message' => 'hello working']);
     }
 }
