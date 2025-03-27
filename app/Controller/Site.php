b2f421c3752ebd9ea20b0bdd0c9e05abab0fd77f
DESKTOP-QL9B2DK\DimASSik <goose9098@gmail.com>
2025-03-27 09:12:04 +0300

    Создал пост пхп для вывода инфы на странице, и модель поста,изменил метод индекс в файле сайта,в настройках добавил метод получения инфы с бд

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,16 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Model\Post;
 use Src\View;
 class Site
 {
-    public function index()
+    public function index() :string
     {
-        $view = new View();
-        return $view->render('site/hello', ['message' => 'index working']);
-
+        $posts = Post::all();
+        return (new View())->render('site.post', ['posts' => $posts]);
     }
-    public function hello()
+    public function hello() :string
     {
         $view = new View();
         return $view->render('site/hello', ['message' => 'hello working']);
--- core/Src/Application.php
+++ core/Src/Application.php
@@ -9,10 +9,13 @@ class Application
     private Settings $settings;
     private Route $route;
 
+    private Capsule $dbManager;
+
     public function __construct(Settings $settings)
     {
         $this->settings = $settings;
         $this->route = new Route();
+        $this->dbManager = new Capsule();
     }
 
     public function __get($key)
@@ -23,8 +26,17 @@ class Application
         throw new Error('Accessing a non-existent property');
     }
 
+    private function dbRun()
+    {
+        $this->dbManager->addConnection($this->settings->getDbSetting());
+        $this->dbManager->setEventDispatcher(new Dispatcher(new Container));
+        $this->dbManager->setAsGlobal();
+        $this->dbManager->bootEloquent();
+    }
+
     public function run(): void
     {
+        $this->dbRun();
         $this->route->setPrefix($this->settings->getRootPath());
         $this->route->start();
     }
