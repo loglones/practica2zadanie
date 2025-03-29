3d67ce181fcbbab5be6aae7ffaddc483b0a9e2d1
DESKTOP-QL9B2DK\DimASSik <goose9098@gmail.com>
2025-03-29 12:25:04 +0300

    удалил весь код,добавил свой шаблон главной страницы,добавил css создал файл контроллер для авторизаци

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Src\Route;
-
-Route::add('GET', '/hello', [App\Controller\Site::class, 'hello'])
-    ->middleware('auth');
-Route::add(['GET', 'POST'], '/signup', [App\Controller\Site::class, 'signup']);
-Route::add(['GET', 'POST'], '/login', [App\Controller\Site::class, 'login']);
-Route::add('GET', '/logout', [App\Controller\Site::class, 'logout']);
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -2,32 +2,52 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pop it MVC</title>
+    <title><?= $title ?? 'Pop it MVC' ?></title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-<header>
-    <nav>
-        <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
-        <?php
-        if (!app()->auth::check()):
-            ?>
-            <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-            <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
-        <?php
-        else:
-            ?>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-        <?php
-        endif;
-        ?>
-    </nav>
+<header class="contForBackgroundPhoto">
+    <div class="wraper firstBlockOfHeader">
+        <div class="contForButtonLogin">
+            <?php if (app()->auth::check()): ?>
+                <button class="headerButtonLogin" onclick="window.location.href='<?= app()->route->getUrl('/logout') ?>'">Выход</button>
+            <?php else: ?>
+                <button class="headerButtonLogin" onclick="window.location.href='<?= app()->route->getUrl('/login') ?>'">Вход</button>
+            <?php endif; ?>
+        </div>
+        <div class="mainButtonsForSearchAndMakeStudents">
+            <div class="upperRowForButtons">
+                <div class="contForButtonMakeStudents">
+                    <button class="headerButton" onclick="window.location.href='<?= app()->route->getUrl('/makeStudents') ?>'">Создать студента</button>
+                </div>
+                <div class="contForButtonMakeStudents">
+                    <button class="headerButton" onclick="window.location.href='<?= app()->route->getUrl('/makeGroup') ?>'">Создание группы</button>
+                </div>
+                <div class="contForButtonMakeStudents">
+                    <button class="headerButton" onclick="window.location.href='<?= app()->route->getUrl('/makeDisciplene') ?>'">Создание дисциплины</button>
+                </div>
+            </div>
+            <div class="lowerRowForButtons">
+                <div class="contForButtonCheckMarkStudent">
+                    <button class="headerButton" onclick="window.location.href='<?= app()->route->getUrl('/checkMarkForDiscipleneAndHours') ?>'">Выбрать успеваемость студента</button>
+                </div>
+                <div class="contForButtonCheckMarkGroupOrDisciplene">
+                    <button class="headerButton" onclick="window.location.href='<?= app()->route->getUrl('/checkStudentForGroupAndDisciplene') ?>'">Выбрать успеваемость по группам</button>
+                </div>
+                <div class="contForButtonMakeGroup">
+                    <button class="headerButton" onclick="window.location.href='<?= app()->route->getUrl('/checkDisciplene') ?>'">Выбрать дисциплину</button>
+                </div>
+            </div>
+        </div>
+        <div class="contForLogo">
+            <a href="<?= app()->route->getUrl('/') ?>"><img class="logo" src="/image/graduation.png" alt="Логотип"></a>
+        </div>
+    </div>
 </header>
 <main>
     <?= $content ?? '' ?>
 </main>
-
 </body>
 </html>
